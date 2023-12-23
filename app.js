4eb8e262ec2bb4967be0576443f68f44f54ea2c3
Prashant Chaudhary <104970128+prashant369-designer@users.noreply.github.com>
2023-12-23 18:38:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+    //      let point1=0;
+    //      let point2=0;
+    //      for(i=1;i<=3;i++){
+            
+    //     let user1=prompt(" pls enter user1 enter 1 for snake and 2 for water and 3 for gun")
+    //     user1 = Number.parseInt(user1);
+    //      let user2=prompt(" pls enter user 2 enter 1 for snake and 2 for water and 3 for gun")
+    //      user2 = Number.parseInt(user2);
+    //      console.log(user1)
+    //      console.log(user2)
+    //     if((user1==1 && user2==1) || (user1==2 && user2==2) || (user1==3 && user2==3)){
+    //        continue;
+    //     }
+    //     else if(user1==2&&user2==3 || user1==3&&user2==2){
+    //       point2=point2+1;
+    //     }
+    //     else if(user1==3&&user2==1 ||user1==1&&user2==3 ){
+    //       point2=point2+1;
+    //     }
+    //     else if(user1==1&&user2==2 || user1==2&&user2==1){
+    //       point1=point1+1;
+    //     }
+    //     else if(user1==1&&user2==3 || user1==3&&user2==1){
+    //       point2=point2+1;
+    //     }
+    // }
+    // if(point2>point1){
+    //   let h1=document.querySelector("h1");
+    //     console.log("user2 wins")
+    //     h1.innerText="Congrats user 2 win";
+    // }
+    // else{
+    //     console.log("user1 wins")
+    // }
+    //     console.log("ended");
+    
